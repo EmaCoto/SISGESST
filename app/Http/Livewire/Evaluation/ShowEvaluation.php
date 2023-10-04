db8cc900222dd969b2d4f8a0cff62d8d728e9c5f
Santiago Alexis Gutierrez Usuga <santiyalexis32@gmail.com>
2023-10-04 16:18:32 -0500

    cree un componente para almacenar el navbar del show-company

--- app/Http/Livewire/Evaluation/ShowEvaluation.php
+++ app/Http/Livewire/Evaluation/ShowEvaluation.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Livewire\Evaluation;
 
+use App\Models\Company;
 use Livewire\Component;
 
 class ShowEvaluation extends Component
 {
+    public $company, $activity, $task;
+    public $openProcesses = [];
+    protected $listeners = ['render'];
+
+    public function mount($id)
+    {
+        $this->company = Company::find($id);
+    }
     public function render()
     {
         return view('livewire.evaluation.show-evaluation');
--- resources/views/livewire/evaluation/show-evaluation.blade.php
+++ resources/views/livewire/evaluation/show-evaluation.blade.php
@@ -1,3 +1,6 @@
 <div>
-    <h1>Hola</h1>
+    <x-navbar-company :company="$company"/>
+
+
+    <script src="https://unpkg.com/flowbite@1.3.4/dist/flowbite.js"></script>
 </div>
